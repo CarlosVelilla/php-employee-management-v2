04f127f0b25dbc72d7fd3f8c0981b811dcede50c
AranBeitia <arantza.beitia@gmail.com>
2021-11-29 11:29:51 +0100

    start

--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -4,58 +4,52 @@ include_once 'employeeManager.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 if (isset($_GET["form"])) {
-    $method = "PUT";
+  $method = "PUT";
 }
-
-
 
 /**
  * Create a New Employee
  */
 if ($method == 'POST') {
-    addEmployee($_REQUEST);
+  addEmployee($_REQUEST);
 }
 
 /**
  * Update Employee
  */
 if ($method == 'PUT') {
-    $employeeFix = [];
-    $array_employee = file_get_contents('php://input');
-    /*     var_dump(intval($_update['id'])); 
+  $employeeFix = [];
+  $array_employee = file_get_contents('php://input');
+  /*     var_dump(intval($_update['id'])); 
     die(); */
-    $array_employee = explode('&', $array_employee);
+  $array_employee = explode('&', $array_employee);
 
-    foreach ($array_employee as $index => $item) {
-        $fixDate = explode('=', urldecode($item));
-        if ($fixDate[0] == 'id') {
-            $employeeFix[$fixDate[0]] = intval($fixDate[1]);
-        } else {
-            $employeeFix[$fixDate[0]] = $fixDate[1];
-        }
+  foreach ($array_employee as $index => $item) {
+    $fixDate = explode('=', urldecode($item));
+    if ($fixDate[0] == 'id') {
+      $employeeFix[$fixDate[0]] = intval($fixDate[1]);
+    } else {
+      $employeeFix[$fixDate[0]] = $fixDate[1];
     }
-    $edit = updateEmployee($employeeFix);
-    // if (isset($_GET["form"])) {
-    //     header('Location: ../dashboard.php');
-    // }
+  }
+  $edit = updateEmployee($employeeFix);
+  // if (isset($_GET["form"])) {
+  //     header('Location: ../dashboard.php');
+  // }
 }
-
-
 
 if ($method == 'PATCH') {
 }
 
 if ($method == 'GET') {
-    $getEmployee = getEmployee($_GET['id']);
-    //header('Location: ../employee.php?id='.$_GET['id']); 
+  $getEmployee = getEmployee($_GET['id']);
+  //header('Location: ../employee.php?id='.$_GET['id']); 
 }
-
-
 
 /**
  * Delete Employee
  */
 if ($method == 'DELETE') {
-    $_delete = file_get_contents('php://input');
-    $delete = deleteEmployee(substr($_delete, 3));
+  $_delete = file_get_contents('php://input');
+  $delete = deleteEmployee(substr($_delete, 3));
 }
--- src/library/employeeManager.php
+++ src/library/employeeManager.php
@@ -9,99 +9,99 @@
 
 function addEmployee(array $newEmployee)
 {
-    $employeesCollection = json_decode(file_get_contents('../../resources/employees.json'), true); //convierte a varible de php (array)
-    $newEmployee['id'] = getNextIdentifier($employeesCollection);
-    $newEmployee['age'] = intval($newEmployee['age']);
+  $employeesCollection = json_decode(file_get_contents('../../resources/employees.json'), true); //convierte a varible de php (array)
+  $newEmployee['id'] = getNextIdentifier($employeesCollection);
+  $newEmployee['age'] = intval($newEmployee['age']);
 
-    if (!isset($newEmployee['gender'])) {
-        $newEmployee['gender'] = "";
-    }
-    if (!isset($newEmployee['lastName'])) {
-        $newEmployee['lastName'] = "";
-    }
+  if (!isset($newEmployee['gender'])) {
+    $newEmployee['gender'] = "";
+  }
+  if (!isset($newEmployee['lastName'])) {
+    $newEmployee['lastName'] = "";
+  }
 
-    array_push($employeesCollection, $newEmployee);
+  array_push($employeesCollection, $newEmployee);
 
-    file_put_contents('../../resources/employees.json', json_encode($employeesCollection, JSON_PRETTY_PRINT));
+  file_put_contents('../../resources/employees.json', json_encode($employeesCollection, JSON_PRETTY_PRINT));
 
-    if (isset($_POST['lastName'])) {
+  if (isset($_POST['lastName'])) {
 
-        session_start();
-        $_SESSION['notification'] = "Inserted Succesful!";
-        header('Location: ../dashboard.php');
-    } else {
-        return $newEmployee['id'];
-    }
+    session_start();
+    $_SESSION['notification'] = "Inserted Succesful!";
+    header('Location: ../dashboard.php');
+  } else {
+    return $newEmployee['id'];
+  }
 }
 
 function deleteEmployee(string $id)
 {
-    $employeesCollection = json_decode(file_get_contents('../../resources/employees.json'), true); //convierte a varible de php (array)
-    // var_dump($employeesCollection);
+  $employeesCollection = json_decode(file_get_contents('../../resources/employees.json'), true); //convierte a varible de php (array)
+  // var_dump($employeesCollection);
 
-    for ($i = 0; $i < count($employeesCollection); $i++) {
-        if ($employeesCollection[$i]['id'] == $id) {
-            // echo $i;
-            array_splice($employeesCollection, $i, 1);
-        } else {
-            echo "not";
-        }
+  for ($i = 0; $i < count($employeesCollection); $i++) {
+    if ($employeesCollection[$i]['id'] == $id) {
+      // echo $i;
+      array_splice($employeesCollection, $i, 1);
+    } else {
+      echo "not";
     }
+  }
 
-    file_put_contents('../../resources/employees.json', json_encode($employeesCollection, JSON_PRETTY_PRINT));
-    return true;
+  file_put_contents('../../resources/employees.json', json_encode($employeesCollection, JSON_PRETTY_PRINT));
+  return true;
 }
 
 function updateEmployee(array $updateEmployee)
 {
-    $employeesCollection = json_decode(file_get_contents('../../resources/employees.json'), true); //convierte a varible de php (array)
+  $employeesCollection = json_decode(file_get_contents('../../resources/employees.json'), true); //convierte a varible de php (array)
 
-    foreach ($employeesCollection as $index => $employee) {
+  foreach ($employeesCollection as $index => $employee) {
 
-        if ($employee['id'] == $updateEmployee['id']) {
-            $employeesCollection[$index] = $updateEmployee;
-        }
+    if ($employee['id'] == $updateEmployee['id']) {
+      $employeesCollection[$index] = $updateEmployee;
     }
-    file_put_contents('../../resources/employees.json', json_encode($employeesCollection, JSON_PRETTY_PRINT));
-    if (isset($_GET["form"])) {
-        header('Location: ../dashboard.php');
-    }
-    return true;
+  }
+  file_put_contents('../../resources/employees.json', json_encode($employeesCollection, JSON_PRETTY_PRINT));
+  if (isset($_GET["form"])) {
+    header('Location: ../dashboard.php');
+  }
+  return true;
 }
 
 function getEmployee(string $id)
 {
-    $employeesCollection = json_decode(file_get_contents('../../resources/employees.json'), true); //convierte a varible de php (array)
+  $employeesCollection = json_decode(file_get_contents('../../resources/employees.json'), true); //convierte a varible de php (array)
 
-    foreach ($employeesCollection as $index => $employee) {
+  foreach ($employeesCollection as $index => $employee) {
 
-        if ($employee['id'] == $id) {
-            echo json_encode($employee);
-        }
+    if ($employee['id'] == $id) {
+      echo json_encode($employee);
     }
-    return false;
+  }
+  return false;
 }
 
 function removeAvatar($id)
 {
-    // TODO implement it
+  // TODO implement it
 }
 
 function getQueryStringParameters(): array
 {
-    // TODO implement it
+  // TODO implement it
 
-    return [];
+  return [];
 }
 
 function getNextIdentifier(array $employeesCollection): int
 {
-    $employeesCollection = json_decode(file_get_contents('../../resources/employees.json'), true); //convierte a varible de php (array)
+  $employeesCollection = json_decode(file_get_contents('../../resources/employees.json'), true); //convierte a varible de php (array)
 
-    $lastId = [];
-    for ($i = 0; $i < count($employeesCollection); $i++) {
-        array_push($lastId, $employeesCollection[$i]['id']);
-    }
+  $lastId = [];
+  for ($i = 0; $i < count($employeesCollection); $i++) {
+    array_push($lastId, $employeesCollection[$i]['id']);
+  }
 
-    return max($lastId) + 1;
+  return max($lastId) + 1;
 }
